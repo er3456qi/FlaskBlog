24c3b886ec1e69c7930669c0b3c4bfc0af758782
er3456qi <er3456qi@outlook.com>
2016-05-30 21:30:21 +0800

    add user auth system

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_moment import Moment
 from config import config
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'  # endpoint of login
 
 
 def create_app(config_name):
@@ -17,8 +21,11 @@ def create_app(config_name):
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')  # add a prefix to route. ex. /auth/login
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import login_required
 
 from . import main
 
@@ -6,3 +7,9 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/user/<username>')
+@login_required
+def user(username):
+    return render_template('user.html')
